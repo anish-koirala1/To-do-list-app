d834aa612016b2d07245ec43caa6ec0c0f6e447e
Anish Koirala <59094@edu.nielsbrock.dk>
2026-06-03 15:35:21 +0200

    Remove duplicate branch paths after merge.
    Keep unified tasks/ and assignments/ modules; drop legacy app/ and public/ trees.

--- app/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// If the web server document root is set to the project root, this file
-// redirects requests into the app/public front controller.
-header("Location: app/public/index.php");
-exit;
